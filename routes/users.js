167a4add3e248f5cde50c074238a1af282ae0f2c
Saranjith p <saranvlmna@gmail.com>
2022-03-25 01:10:28 +0530

    base

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,13 @@
 var express = require('express');
+var productcontroller = require('../controller/product-controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  let products = [
-    {
-      name: "Iphone",
-      category: 'mobile',
-      price: 1000,
-      description: 'Iphone is a mobile phone from Apple Inc.',
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMPihIju0BBCwq0HWOFF-aZo8vlz2FCOl7fg&usqp=CAU"
-    },
-    {
-      name: "Iphone",
-      category: 'mobile',
-      price: 1000,
-      description: 'Iphone is a mobile phone from Apple Inc.',
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMPihIju0BBCwq0HWOFF-aZo8vlz2FCOl7fg&usqp=CAU"
-    }
-  ]
+  productcontroller.getAllProducts().then((products)=>{
+    res.render('users/view-product.hbs', { admin: false, products })
+  })
 
-  res.render('index', { products, admin: false });
 });
 module.exports = router;
