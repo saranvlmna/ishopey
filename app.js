6048e48402779b73a949d611cf073f432b2c5e89
Saranjith p <61007285+saranvlmna@users.noreply.github.com>
2022-08-04 17:06:37 +0530

    session age updated

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-app.use(session({ secret: 'key', cookie: { maxAge: 600000 } }));
+app.use(session({ secret: 'key', cookie: { maxAge: 365 * 24 * 60 * 60 * 1000 } }));
 
 db.connect((err) => {
   if (err) console.log('Connection Error' + err);
